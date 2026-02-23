63052a3a00c663396aa80fd83381d4901d2eac96
Timbo63 <sharptimal@gmail.com>
2026-02-23 14:51:40 -0500

    Refactor import statements in main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from db.session import Base, engine
-from api.auth import router as auth_router
-from api.companies import router as companies_router
-from api.assessments import router as assessments_router
-from api.routes import (
+from session import Base, engine
+from auth import router as auth_router
+from companies import router as companies_router
+from assessments import router as assessments_router
+from routes import (
     scores_router, opps_router, providers_router,
     matches_router, dashboard_router,
 )
