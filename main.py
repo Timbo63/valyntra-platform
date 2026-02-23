bffb8f46fe525d60e79a756a1b9f1ea3e7581910
Timbo63 <sharptimal@gmail.com>
2026-02-23 18:19:08 -0500

    Change CORS settings to allow all origins

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://valyntra.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
